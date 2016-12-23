311e9cf599f0c698f2d38f2d48ec738f8205aeab
Yong He <yonghe@outlook.com>
2016-12-22 21:35:56 -0500

    Delete updateSpireLib.bat

--- updateSpireLib.bat
+++
@@ -1,3 +0,0 @@
-xcopy /r /i /s /y /exclude:codeonlyexcludelist.txt ..\spire2\Spire\Source GameEngineCore\Spire\Source
-copy /y ..\spire2\Spire\Spire.h GameEngineCore\Spire\Spire.h
-copy /y ..\spire2\Spire\SpireAllSource.h GameEngineCore\Spire\SpireAllSource.h
