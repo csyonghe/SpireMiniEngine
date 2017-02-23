272570b095cbb6fe38cada9f01782cc42e6008c2
Yong He <yonghe@outlook.com>
2017-02-22 20:09:02 -0500

    update

--- GameEngineCore/SystemWindow.h
+++ GameEngineCore/SystemWindow.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef GAME_ENGINE_SYSTEM_WINDOW_H
+#define GAME_ENGINE_SYSTEM_WINDOW_H
+
+#include "CoreLib/WinForm/WinForm.h"
+#include "LibUI/LibUI.h"
+#include "HardwareRenderer.h"
+
+namespace GameEngine
+{
+    class SystemWindow : public CoreLib::WinForm::Form
+    {
+    private:
+        CoreLib::RefPtr<GraphicsUI::UIEntry> uiEntry;
+        CoreLib::RefPtr<WindowSurface> surface;
+    };
+}
+
+#endif
