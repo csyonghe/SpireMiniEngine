4c259e1cfaff279409c3895188bf9464481b76eb
YONGH\yongh <yonghe@outlook.com>
2017-10-19 19:16:39 -0400

    adding prepare script to download external dependencies.

---
+++ prepare.ps1
@@ -0,0 +1,10 @@
+if (-NOT ((Test-Path '.\ExternalLibs\FbxSDK\x64\debug\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x64\release\libfbxsdk.lib') -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\release\libfbxsdk.lib')  -AND (Test-Path '.\ExternalLibs\FbxSDK\x86\debug\libfbxsdk.lib'))) {
+"Required binaries not found, downloading..."
+(New-Object Net.WebClient).DownloadFile('https://github.com/csyonghe/SpireMiniEngineExtBinaries/raw/master/binaries.zip', 'binaries.zip')
+Add-Type -assembly "system.io.compression.filesystem"
+Remove-Item -Recurse -Force 'ExternalLibs'
+[System.IO.Compression.ZipFile]::ExtractToDirectory("binaries.zip", '.\')
+Remove-Item 'binaries.zip'
+}
+"Done."
+cmd /c pause | out-null
